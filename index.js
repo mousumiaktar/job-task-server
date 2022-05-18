68715ca35305cb7f650ff41286b2457bd3e65ceb
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2022-05-18 23:16:13 +0600

    get api added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,6 +10,31 @@ app.use (cors());
 app.use(express.json());
 
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3kcha.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const userCollection = client.db('teamUser').collection('user');
+
+        app.get('/user', async(req, res)=>{
+            const query = {};
+            const cursor = userCollection.find(query);
+            const users = await cursor.toArray();
+            res.send(users);
+        });
+
+
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
 
 
 
