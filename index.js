b21be87782b8edd57be8351706940fa244f82dd5
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2022-05-19 02:03:27 +0600

    delete api added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -31,6 +31,15 @@ async function run(){
             const newUser = req.body;
             console.log(newUser);
             const result = await userCollection.insertOne(newUser);
+            res.send(result);
+        })
+
+
+        app.delete('/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            console.log(query);
+            const result = await userCollection.deleteOne(query);
             res.send(result);
         })
 
